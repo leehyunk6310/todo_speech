6586a0fa5c4d8c1f79411ab8cf0c9c42034dd6a7
lee <lee@gmail.com>
2023-06-12 14:35:12 +0900

    ft : add navi items

---
+++ lib/calendar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+                'calendar'
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
